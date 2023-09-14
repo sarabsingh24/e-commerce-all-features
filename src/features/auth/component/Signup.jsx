5b1d076cfc5390c90705d730ea50cdc4a782bd82
sarabsingh24 <sarabpreet33@gmail.com>
2023-09-14 12:47:50 +0530

    first push  to github

--- src/features/auth/component/Signup.jsx
+++ src/features/auth/component/Signup.jsx
@@ -44,8 +44,6 @@ const Signup = () => {
 
   return (
     <React.Fragment>
-      {/* {user?.email && <Navigate to="/login" />} */}
-
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
