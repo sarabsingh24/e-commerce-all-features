50250339fe5e956198a1d89d739a22399832cbde
sarabsingh24 <sarabpreet33@gmail.com>
2023-09-14 12:58:27 +0530

    passwort type

--- src/features/auth/component/Signup.jsx
+++ src/features/auth/component/Signup.jsx
@@ -140,7 +140,7 @@ const Signup = () => {
                     validate: (value, formValues) =>
                       value === formValues.password || 'password not matched',
                   })}
-                  type="password"
+                  // type="password"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
 
